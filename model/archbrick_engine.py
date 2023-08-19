f34ed013e27689868cb6167ad8d3976577339aca
Danger_is-real <dodger666@gmail.com>
2023-08-19 12:04:51 +0200

    ocean algo done

--- model/archbrick_engine.py
+++ model/archbrick_engine.py
@@ -43,10 +43,11 @@ class ArchbrickEngine:
         grid_map = np.ndarray(shape=(self.width, self.height), dtype=Hex)
         text_mapper = ""
         self._step_1_init_map(grid_map)
-        self._step_2_ocean(grid_map)
-        self._step_3_extend_ocean(grid_map)
+        sides_with_ocean = self._step_2_ocean(grid_map)
+        self._step_3_extend_ocean(grid_map, sides_with_ocean)
+        self._step_4_extend_secondary_ocean(grid_map, sides_with_ocean)
 
-        # final formating for text-mapper
+        # final formatting for text-mapper
         for col in grid_map:
             for hex in col:
                 text_mapper = text_mapper + f'{hex.col + 1:02}{hex.row + 1:02} {hex.terrain.value}' + '\n'
@@ -57,11 +58,9 @@ class ArchbrickEngine:
         h = range(0, self.height)
         w = range(0, self.width)
         for col in w:
-            # grid_map = np.append(grid_map, [])
             for row in h:
                 current_hex = Hex(col=col, row=row)
                 current_hex.text_mapper = f'{col + 1:02}{row + 1:02} {current_hex.terrain.value}'
-                # grid_map = np.append(grid_map[col], current_hex)
                 grid_map[col, row] = current_hex
 
     def _step_2_ocean(self, grid_map):
@@ -72,58 +71,118 @@ class ArchbrickEngine:
         print('shore sides:', sides_has_ocean)
         for side in sides_has_ocean:
             if side == 1:
-                for col in grid_map:
-                    col[0].terrain = TerrainType.ocean
-            if side == 3:
-                for col in grid_map:
-                    col[-1].terrain = TerrainType.ocean
-            if side == 2:
-                for row in grid_map[-1]:
-                    row.terrain = TerrainType.ocean
-            if side == 4:
-                for row in grid_map[0]:
-                    row.terrain = TerrainType.ocean
-
-    def _step_3_extend_ocean(self, grid_map):
-        for col in grid_map[1:-2]:
-            for hex in col[1:-2]:
-                if self._is_near(hex, TerrainType.ocean, grid_map) and (dice.roll("1d6").pop() <= 3) and not hex.is_done:
+                for hex in grid_map[:, 0]:
                     hex.terrain = TerrainType.ocean
-                    print(f"hex:{hex.col},{hex.row} is ocean")
-                hex.is_done = True
+            if side == 3:
+                for hex in grid_map[:, -1]:
+                    hex.terrain = TerrainType.ocean
+            if side == 2:
+                for hex in grid_map[-1, :]:
+                    hex.terrain = TerrainType.ocean
+            if side == 4:
+                for hex in grid_map[0, :]:
+                    hex.terrain = TerrainType.ocean
+        return sides_has_ocean
+
+    def _step_3_extend_ocean(self, grid_map, sides_with_ocean):
+        print('step3')
+        if not sides_with_ocean:
+            return
+        if 1 in sides_with_ocean:
+            for hex in grid_map[:, 1]:
+                self.chance_to_be(grid_map, hex, TerrainType.ocean, 3)
+        if 3 in sides_with_ocean:
+            for hex in grid_map[:, -2]:
+                self.chance_to_be(grid_map, hex, TerrainType.ocean, 3)
+        if 2 in sides_with_ocean:
+            for hex in grid_map[-2, :]:
+                self.chance_to_be(grid_map, hex, TerrainType.ocean, 3)
+        if 4 in sides_with_ocean:
+            for hex in grid_map[1, :]:
+                self.chance_to_be(grid_map, hex, TerrainType.ocean, 3)
+
+    def _step_4_extend_secondary_ocean(self, grid_map, sides_with_ocean):
+        print('step4')
+        if not sides_with_ocean:
+            return
+        if 1 in sides_with_ocean:
+            for hex in grid_map[:, 2]:
+                self.chance_to_be(grid_map, hex, TerrainType.ocean, 2)
+        if 3 in sides_with_ocean:
+            for hex in grid_map[:, -3]:
+                self.chance_to_be(grid_map, hex, TerrainType.ocean, 2)
+        if 2 in sides_with_ocean:
+            for hex in grid_map[-3, :]:
+                self.chance_to_be(grid_map, hex, TerrainType.ocean, 2)
+        if 4 in sides_with_ocean:
+            for hex in grid_map[2, :]:
+                self.chance_to_be(grid_map, hex, TerrainType.ocean, 2)
+
+    def chance_to_be(self, grid_map, hex: Hex, terrain: TerrainType, x_chance: int):
+        if self._is_near(hex, terrain, grid_map) and self._x_chance_in_6(x_chance) and not hex.is_done:
+            hex.terrain = terrain
+            print(f"hex:{hex.col + 1},{hex.row + 1} is ocean")
+        hex.is_done = True
 
     def _is_near(self, hex: Hex, terrain_type: TerrainType, gridmap):
-        try:
-            if gridmap[hex.col][hex.row-1].terrain == terrain_type:
+
+        def is_out_of_bounds(col: int, row:int):
+            if col < 0 or col >= self.width:
                 return True
-        except:
-            pass
-        try:
-            if gridmap[hex.col+1][hex.row - 1].terrain == terrain_type:
+            if row < 0 or row >= self.height:
                 return True
-        except:
-            pass
-        try:
-            if gridmap[hex.col+1][hex.row].terrain == terrain_type:
-                return True
-        except:
-            pass
-        try:
-            if gridmap[hex.col][hex.row + 1].terrain == terrain_type:
-                return True
-        except:
-            pass
-        try:
-            if gridmap[hex.col - 1][hex.row + 1].terrain == terrain_type:
-                return True
-        except:
-            pass
-        try:
-            if gridmap[hex.col - 1][hex.row - 1].terrain == terrain_type:
-                return True
-        except:
-            pass
+            return False
+
+        def is_even_col(col: int):
+            return (col % 2) == 0
+
+        def check_one_near_hex(hex: Hex, gridmap, col_offset:int, row_offset:int, terrain_type: TerrainType):
+            if not is_out_of_bounds(hex.col + col_offset, hex.row + row_offset):
+                hex_to_check: Hex = gridmap[hex.col + col_offset, hex.row + row_offset]
+                if hex_to_check.terrain == terrain_type:
+                    print(
+                        f'hex {hex.col + 1},{hex.row + 1} is near hex {hex_to_check.col + 1},{hex_to_check.row + 1},{hex_to_check.terrain.value}')
+                    return True
+
+        # North
+        row_offset = -1
+        col_offset = 0
+        if check_one_near_hex(hex, gridmap, col_offset, row_offset, terrain_type):
+            return True
+
+        # N-E
+        row_offset = -1 if is_even_col(hex.col) else 0
+        col_offset = 1
+        if check_one_near_hex(hex, gridmap, col_offset, row_offset, terrain_type):
+            return True
+
+        # S-E
+        row_offset = 0 if is_even_col(hex.col) else 1
+        col_offset = 1
+        if check_one_near_hex(hex, gridmap, col_offset, row_offset, terrain_type):
+            return True
+
+        # South
+        row_offset = 1
+        col_offset = 0
+        if check_one_near_hex(hex, gridmap, col_offset, row_offset, terrain_type):
+            return True
+
+        # S-W
+        row_offset = 0 if is_even_col(hex.col) else +1
+        col_offset = -1
+        if check_one_near_hex(hex, gridmap, col_offset, row_offset, terrain_type):
+            return True
+        # N-W
+        row_offset = -1 if is_even_col(hex.col) else 0
+        col_offset = -1
+        if check_one_near_hex(hex, gridmap, col_offset, row_offset, terrain_type):
+            return True
+
         return False
+
+    def _x_chance_in_6(self, x_chance: int):
+        return dice.roll("1d6").pop() <= x_chance
 
 
 
