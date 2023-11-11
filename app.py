24b84d17ebf4000f743a40bf10e18516c17e8253
Frédéric Roelandts <59567173+Dodger666@users.noreply.github.com>
2023-11-11 11:46:58 +0100

    change app WSGI app name to 'app' (needed for versel)

--- app.py
+++ app.py
@@ -4,18 +4,18 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from api import characters, maps, whitebox_character, snw_treasure_generator, names, ose_character, swc_character
 
-appapi = FastAPI()
+app = FastAPI()
 
 this_dir = os.path.abspath(os.path.dirname(__file__))
 static_folder = os.path.join(this_dir, 'api/static')
-appapi.mount("/static", StaticFiles(directory=static_folder), name="static")
-appapi.include_router(characters.router, tags=["characters"])
-appapi.include_router(maps.router, tags=["maps"])
-appapi.include_router(whitebox_character.router, tags=["whitebox_character"])
-appapi.include_router(ose_character.router, tags=["ose_character"])
-appapi.include_router(swc_character.router, tags=["swc_character"])
-appapi.include_router(snw_treasure_generator.router, tags=["snw_treasure_generator"])
-appapi.include_router(names.router, tags=["holmesian_names"])
+app.mount("/static", StaticFiles(directory=static_folder), name="static")
+app.include_router(characters.router, tags=["characters"])
+app.include_router(maps.router, tags=["maps"])
+app.include_router(whitebox_character.router, tags=["whitebox_character"])
+app.include_router(ose_character.router, tags=["ose_character"])
+app.include_router(swc_character.router, tags=["swc_character"])
+app.include_router(snw_treasure_generator.router, tags=["snw_treasure_generator"])
+app.include_router(names.router, tags=["holmesian_names"])
 
 
 
